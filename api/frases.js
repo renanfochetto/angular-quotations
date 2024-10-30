39289fceb83765bcba3bf29254968d890eca3972
rnn <145611850+renanfochetto@users.noreply.github.com>
2024-10-30 00:14:44 -0300

    "criação da pasta api"

--- api/frases.js
+++ api/frases.js
@@ -1,15 +1,24 @@
 export default async function handler(req, res) {
   const apiUrl = 'https://zenquotes.io/random';
+  console.log("Chamando a API:", apiUrl);
 
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
+    if (!response.ok) {
+      console.error('Erro ao buscar dados:', response.status, response.statusText);
+      return res.status(response.status).json({ error: 'Erro ao buscar dados' });
+    }
 
-    res.setHeader('Access-Control-Allow-Origin', 'https://angular-visual-quotes.vercel.app/');
+    const data = await response.json();
+    console.log("Dados recebidos:", data);
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'application/json');
 
     res.status(200).json(data);
   } catch (error) {
+    console.error('Erro no processamento:', error);
     res.status(500).json({ error: 'Erro ao buscar frase' });
   }
 }
+
