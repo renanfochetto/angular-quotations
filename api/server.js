4c8fd86f2e0db6968df11770a4ac4e759a92cd7f
rnn <145611850+renanfochetto@users.noreply.github.com>
2024-10-30 14:53:53 -0300

    "Renomeado backend/server para api/server e refatoração do servidor"

---
+++ api/server.js
@@ -0,0 +1,29 @@
+import fetch from "node-fetch";
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../unsplash.env' });
+
+export default async function handler(req, res) {
+  const { method } = req;
+
+  if (method === 'GET' && req.url === '/api/frases') {
+    try {
+      const response = await fetch('https://zenquotes.io/api/random', { method: 'GET' });
+      const data = await response.json();
+      res.status(response.status).json(data);
+    } catch (error) {
+      res.status(500).json( { error: 'Error ao buscar a frase' } );
+    }
+  } else if (method === 'GET' && req.url === '/api/imagem') {
+    const accessKey = process.env.UNSPLASH_ACCESS_KEY;
+    try {
+      const response = await fetch(`https://api.unsplash.com/photos/random?query=landscape&client_id=${accessKey}`, { method: 'GET'});
+      const data = await response.json();
+      res.status(response.status).json(data);
+    } catch (error) {
+      res.status(500).json( { error: 'Erro ao buscar a imagem' } );
+    }
+  } else {
+    res.status(404).json( { error: 'Rota não encontrada. ' } );
+  }
+}
