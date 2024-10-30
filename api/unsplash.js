471ec4a202e61bc9bc951fe840dc6b75187a3a82
rnn <145611850+renanfochetto@users.noreply.github.com>
2024-10-30 19:30:14 -0300

    "Adição de log para verificação de chave de acesso."

--- api/unsplash.js
+++ api/unsplash.js
@@ -6,6 +6,7 @@ dotenv.config({ path: '../unsplash.env' } );
 export default async function handler(req, res) {
   try {
     const accessKey = process.env.UNSPLASH_ACCESS_KEY;
+    console.log('Chave do Unsplash:', accessKey)
     const response = await fetch(`https://api.unsplash.com/photos/random?query=landscape&client_id=${accessKey}`, { method: 'GET'});
     const data = await response.json();
     res.status(response.status).json(data);
