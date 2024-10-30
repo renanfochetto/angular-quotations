6a345940ac542f6ae99c4dd83f0506598f06cb33
rnn <145611850+renanfochetto@users.noreply.github.com>
2024-10-30 16:38:17 -0300

    "Correção de erro no server.js"

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ dotenv.config({ path: '../unsplash.env' });
 
 export default async function handler(req, res) {
 
-  res.setHeader('Access-Control-Allor-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Origin', 'GET');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
