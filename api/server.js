a138281c0a2fbf97988ba5c92b4af56df1361864
rnn <145611850+renanfochetto@users.noreply.github.com>
2024-10-30 17:14:36 -0300

    "Adição de logs no server.js para checagem de endpoint."

--- api/server.js
+++ api/server.js
@@ -4,22 +4,29 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '../unsplash.env' });
 
 export default async function handler(req, res) {
-
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  const { method } = req;
+  const { method, url } = req;
 
-  if (method === 'GET' && req.url === '/api/frases') {
+  console.log('Método:', method);
+  console.log('URL:', url);
+
+  if (method === 'GET' && url === '/api/frases') {
+    console.log('Entrou no bloco: frases');
     try {
+      console.log('Buscando frase da API externa...');
       const response = await fetch('https://zenquotes.io/api/random', { method: 'GET' });
+      console.log('Resposta da API externa:', response.status);
       const data = await response.json();
+      console.log('Dados retornados:', data);
       res.status(response.status).json(data);
     } catch (error) {
       console.log('Erro ao buscar a frase');
       res.status(500).json( { error: 'Error ao buscar a frase' } );
     }
   } else if (method === 'GET' && req.url === '/api/imagem') {
+    console.log('Entrou no bloco: imagem');
     const accessKey = process.env.UNSPLASH_ACCESS_KEY;
     try {
       const response = await fetch(`https://api.unsplash.com/photos/random?query=landscape&client_id=${accessKey}`, { method: 'GET'});
