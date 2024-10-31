8f9c4f7c58e48afc8381886267293a73fea5d7a6
rnn <145611850+renanfochetto@users.noreply.github.com>
2024-10-31 10:32:16 -0300

    "Teste na Função ServerLess"

--- api/unsplash.js
+++ api/unsplash.js
@@ -7,8 +7,8 @@ console.log('Variáveis de ambiente:', process.env);
 
 export default async function handler(req, res) {
   try {
+    console.log('Endpoint /api/imagem foi chamado');
     const accessKey = process.env.UNSPLASH_ACCESS_KEY;
-    console.log('Chave do Unsplash:', accessKey)
     const response = await fetch(`https://api.unsplash.com/photos/random?query=landscape&client_id=${accessKey}`, { method: 'GET'});
     const data = await response.json();
     res.status(response.status).json(data);
