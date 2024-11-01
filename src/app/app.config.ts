11afec6262b0ec47cd495b13cef081409ce5f5d4
rnn <145611850+renanfochetto@users.noreply.github.com>
2024-10-31 21:49:42 -0300

    "Ajustes na build de produção."

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { provideHttpClient } from "@angular/common/http";
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
+
   providers: [
     provideRouter(routes),
     provideHttpClient()
